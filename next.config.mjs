94e7b6713a429f98ac86764ec27ee1e3442df3e2
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-11-01 16:12:39 +0530

    chore: update configs and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: '',
+            pathname: '/dbs9ulw2r/image/upload/**',
+          },
+          {
+            protocol: "https",
+            hostname: "utfs.io",
+            port: "",
+            pathname: "/f/**", 
+          }
+        ],
+      },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,17 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      colors: {
+        charcoalGray: "#121212",
+        darkSlateGray: "#191919",
+        midNightBlue: "#212121",
+      }
     },
   },
   plugins: [],
