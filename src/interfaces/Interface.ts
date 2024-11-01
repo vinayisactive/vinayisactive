cc260282dd3f74844c166af6170a8921e8b14151
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-11-01 16:07:24 +0530

    feat: add TypeScript interfaces in a seprate file

---
+++ src/interfaces/Interface.ts
@@ -0,0 +1,28 @@
+export interface SkillInterface {
+    column: string;
+    technologies: string[];
+}
+
+export interface Projectlink {
+    name: string;
+    url: string;
+}
+
+export interface ProjectInterface {
+    name: string, 
+    logoUrl: string,
+    links: Projectlink[],
+    details : string[],
+}
+
+
+export interface SocialsInterface {
+    name: string;
+    id: string;
+    url: string;
+}
+
+export interface BlogInterface {
+    title: string;
+    url: string; 
+}
