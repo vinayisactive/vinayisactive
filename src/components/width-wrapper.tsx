b7f7373eca186d9f91cbf79d452af904f8ce2e70
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-08-01 03:30:49 +0530

    style: added padding in width wrapper

--- src/components/width-wrapper.tsx
+++ src/components/width-wrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 function WidthWrapper({children, className}: {children: ReactNode; className?:string}){
     return(
-        <main className={`w-full max-w-3xl mx-auto ${className}`}>
+        <main className={`w-full max-w-3xl mx-auto px-4 ${className}`}>
             {children}
         </main>
     )
